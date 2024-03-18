70876e198f29ee42dcc8b806800c43a3a8df82d6
vktmk123 <patheonmk@gmail.com>
2024-03-19 03:08:42 +0700

    connect API

--- my-app/src/components/TableUser.js
+++ my-app/src/components/TableUser.js
@@ -1,8 +1,51 @@
+import { useEffect, useState } from 'react';
+import Table from 'react-bootstrap/Table';
+import {fetchAllUsers} from '../services/UserService';  
+
 const TableUser = (props) => {
+    const[listUsers, setListUser] = useState([]);
+    
+    useEffect(() => {
+        getUsers();
+    }, [])
+
+    const getUsers = async() => {
+        let res = await fetchAllUsers();
+        if(res && res.data && res.data.data){
+            setListUser(res.data.data);
+        }
+        console.log("res: ", res);
+    }
+    console.log("listUsers: ", listUsers);
+
     return (
-        <header>
-            <h1>Table User</h1>
-        </header>
+        <>
+         <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Email</th>
+          <th>First Name</th>
+          <th>Last name</th>
+        </tr>
+      </thead>
+      <tbody>
+        {listUsers && listUsers.length > 0 &&
+
+        listUsers.map((user, index) => {
+          return (
+            <tr key={`index-${index}`}>
+              <td>{user.id}</td>
+              <td>{user.email}</td>
+              <td>{user.first_name}</td>
+              <td>{user.last_name}</td>
+            </tr>
+          )
+        })
+        }
+      </tbody>
+    </Table>
+        </>
     );
 }
 
