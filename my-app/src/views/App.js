a3400255a6a0fcc2b3d95e0264007dbaaa837f99
vktmk123 <patheonmk@gmail.com>
2024-03-20 02:32:03 +0700

    tao add

--- my-app/src/views/App.js
+++ my-app/src/views/App.js
@@ -1,44 +1,28 @@
 import Header from '../components/Header';
 import './App.scss';
 import TableUser from '../components/TableUser';
-import Container from 'react-bootstrap/Container';
-import ModalAddNew from '../components/ModalAddNew';
-import { useState } from 'react';
+import Container from 'react-bootstrap/Container'; 
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 
 const handleSaveUser = () => {
   // some code to save user
-
   toast.success("User saved successfully");
 }
 
 
 function App() {
 
-  const [isShowModalAddNew, setIsShowModalAddNew] = useState(false);
-  const handleClose = () => {
-    setIsShowModalAddNew(false);
-  }
-
-
-
   return (
     <div className='app-container'>
       <Header />
       <Container>
-        <div className='my-3 add-new'>
-          <h3>List User: </h3>
-          <button className='btn btn-primary'
-            onClick={() => setIsShowModalAddNew(true)}>Add User</button>
-        </div>
+    
         <TableUser />
       </Container>
 
-      <ModalAddNew show={isShowModalAddNew}
-        handleClose={handleClose} />
-
+      
 
       <ToastContainer
         position="top-right"
