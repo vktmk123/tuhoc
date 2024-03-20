6228dfa9b6d41a3ba251e89e1ef8c7073ca65603
vktmk123 <patheonmk@gmail.com>
2024-03-20 22:48:43 +0700

    Revert "tao add"
    This reverts commit a3400255a6a0fcc2b3d95e0264007dbaaa837f99.

--- my-app/src/components/TableUser.js
+++ my-app/src/components/TableUser.js
@@ -1,100 +1,82 @@
-import { useEffect, useState } from "react";
-import Table from "react-bootstrap/Table";
-import { fetchAllUsers } from "../services/UserService";
-import ReactPaginate from "react-paginate";
-import ModalAddNew from "./ModalAddNew";
+import { useEffect, useState } from 'react';
+import Table from 'react-bootstrap/Table';
+import {fetchAllUsers} from '../services/UserService';  
+import ReactPaginate from 'react-paginate';
 
 const TableUser = (props) => {
-  const [listUsers, setListUsers] = useState([]);
-  const [totalUsers, setTotalUsers] = useState(0);
-  const [totalPages, setTotalPages] = useState(0);
+    const[listUsers, setListUser] = useState([]);
+    const[totalUsers, setTotalUsers] = useState(0);
+    const[totalPages, setTotalPages] = useState(0);
 
-  const [isShowModalAddNew, setIsShowModalAddNew] = useState(false);
 
-  const handleClose = () => {
-    setIsShowModalAddNew(false);
-  };
+    useEffect(() => {
+      //call api
+      getUsers(1);}, [])
 
-  const handleUpdateTable = (user) => {
-    setListUsers([user, ...listUsers]);
-  };
-
-  useEffect(() => {
-    //call api
-    getUsers(1);
-  }, []);
-
-  const getUsers = async (page) => {
-    let res = await fetchAllUsers(page);
-    if (res && res.data) {
-      setTotalUsers(res.total);
-      setTotalPages(res.total_pages);
-      setListUsers(res.data);
+    const getUsers = async(page) => {
+        let res = await fetchAllUsers(page);
+        if(res && res.data){
+            setTotalUsers(res.total);
+            setTotalPages(res.total_pages);
+            setListUser(res.data);
+        }
+        console.log("res: ", res);
     }
-    console.log("res: ", res);
-  };
+    
+    const handlePageClick = async (event) => {
+      getUsers(+event.selected+1);
+      console.log("event lib:", event)
+    }
 
-  const handlePageClick = async (event) => {
-    getUsers(+event.selected + 1);
-    console.log("event lib:", event);
-  };
+    return (
+        <>
+         <Table striped bordered hover>
 
-  return (
-    <>
-      <div className="my-3 add-new">
-        <h3>List User: </h3>
-        <button
-          className="btn btn-primary"
-          onClick={() => setIsShowModalAddNew(true)}
-        > Add User </button>
-      </div>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Email</th>
-            <th>First Name</th>
-
-          </tr>
-        </thead>
-        <tbody>
-          {listUsers && listUsers.length > 0 && listUsers.map((user, index) => {
-              return (
-                <tr key={`users-${index}`}>
-                  <td>{user.id}</td>
-                  <td>{user.email}</td>
-                  <td>{user.first_name}</td> 
-                </tr>
-              );
-            })}
-        </tbody>
-      </Table>
-      <ReactPaginate
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Email</th>
+          <th>First Name</th>
+          <th>Last name</th>
+        </tr>
+      </thead>
+      <tbody>
+        {listUsers && listUsers.length > 0 && listUsers.map((user, index) => {
+          return (
+            <tr key={`index-${index}`}>
+              <td>{user.id}</td>
+              <td>{user.email}</td>
+              <td>{user.first_name}</td>
+              <td>{user.last_name}</td>
+            </tr>
+          )
+        })
+        }
+      </tbody>
+    </Table>
+    <ReactPaginate
         breakLabel="..."
         nextLabel="next >"
-        onPageChange={handlePageClick}
+        onPageChange={handlePageClick} 
         pageRangeDisplayed={5}
         pageCount={totalPages}
         previousLabel="< previous"
         renderOnZeroPageCount={null}
-        pageClassName="page-item"
-        pageLinkClassName="page-link"
-        previousClassName="page-item"
-        previousLinkClassName="page-link"
-        nextClassName="page-item"
-        nextLinkClassName="page-link"
-        breakClassName="page-item"
-        breakLinkClassName="page-link"
-        containerClassName="pagination"
-        activeClassName="active"
+        pageClassName='page-item'
+        pageLinkClassName='page-link'
+        previousClassName='page-item'
+        previousLinkClassName='page-link'
+        nextClassName='page-item'
+        nextLinkClassName='page-link'
+        breakClassName='page-item'
+        breakLinkClassName='page-link'
+        containerClassName='pagination'
+        activeClassName='active'
       />
-      <ModalAddNew
-        show={isShowModalAddNew}
-        handleClose={handleClose}
-        handleUpdateTable={handleUpdateTable}
-      />
-    </>
-  );
-};
+    
+
+        </>
+    );
+}
 
 export default TableUser;
